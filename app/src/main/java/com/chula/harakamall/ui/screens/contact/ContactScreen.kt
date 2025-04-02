ce1208e0bd383b592e144104c0c53429f3925bcb
chula247 <omondijohn448@gmail.com>
2025-04-02 13:46:19 +0300

    and another one

---
+++ app/src/main/java/com/chula/harakamall/ui/screens/contact/ContactScreen.kt
@@ -0,0 +1,17 @@
+package com.chula.harakamall.ui.screens.contact
+
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun HomeScreen(){
+
+
+}
+
+@Preview
+@Composable
+fun HomeScreenPreview(){
+
+}

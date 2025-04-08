88da90e5d5fdee4a992739f577fc707416f18054
johnyyyyy-de <omondijohn448@gmail.com>
2025-04-08 13:31:42 +0300

    Change

--- app/src/main/java/com/chula/harakamall/navigation/AppNavHost.kt
+++ app/src/main/java/com/chula/harakamall/navigation/AppNavHost.kt
@@ -7,6 +7,7 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.chula.harakamall.ui.screens.about.AboutScreen
+import com.chula.harakamall.ui.screens.dashboard.DashboardScreen
 import com.chula.harakamall.ui.screens.home.HomeScreen
 import com.chula.harakamall.ui.screens.intent.IntentScreen
 import com.chula.harakamall.ui.screens.item.ItemScreen
@@ -16,7 +17,7 @@ import com.chula.harakamall.ui.screens.start.StartScreen
 fun AppNavHost(
     modifier: Modifier = Modifier,
     navController: NavHostController = rememberNavController(),
-    startDestination: String = ROUT_START
+    startDestination: String = ROUT_DASHBOARD
 ) {
 
     NavHost(
@@ -40,6 +41,10 @@ fun AppNavHost(
 
         composable(ROUT_INTENT) {
             IntentScreen(navController)
+        }
+
+        composable(ROUT_DASHBOARD) {
+            DashboardScreen(navController)
         }
 
     }
